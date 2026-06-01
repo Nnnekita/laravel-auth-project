d3faf7cb85cd690f2cfed3b9860e70f0ae9e4466
Nnnekita <kosmit.play@mail.ru>
2026-06-01 15:25:54 +0300

    Initial commit: Laravel project with Tailwind CSS v4

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            assets: ['resources/images/**'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: 'ezuphac-m4.webvgppk.loc',
+    }
+});
